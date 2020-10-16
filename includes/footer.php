5de2128b0d61a528026a7043ffd20b4586aeb345
Revonda Mcleod <revondamcleod@gmail.com>
2020-10-16 11:04:00 -0500

    final

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 </div>
 <?php
-        echo "<p>Copyright &copy; 1888-" . date("Y") . "PHP STUDENT</p>";
+        echo "<p>Copyright &copy; 1888-" . date("Y") . " PHP REVONDA</p>";
         
 ?>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
