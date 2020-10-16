a3c3ecc5c5cb9afcf47aaf2600ce3ee6a3aeefb1
Revonda Mcleod <revondamcleod@gmail.com>
2020-10-16 10:22:16 -0500

    Update header.php
    Fix Title

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=<php, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">    
-    <title><?php echo "$title"?></title>
+    <title><?php echo "PHP Primer-"."$title"?></title>
 </head>
 
 <body>
